dc95d6141aa4fe11cf404a463e3c5b924e392ad7
Brian Harris <brianharrisdesign@gmail.com>
2017-10-24 15:54:29 -0700

    login ready to hook up to server

---
+++ warbler-client/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+    <div className="row header align-items-center">
+      <div className="header-title col-4 offset-4">
+        Warbler
+      </div>
+    </div>
+);
+
+export default Header;
